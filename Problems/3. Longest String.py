898088fd5258d439d90198cb70c75c6d1d3a43a2
SpacePirate <dyoon1992@gmail.com>
2019-05-11 22:19:16 -0700

    Problem 3 submitted

---
+++ Problems/3. Longest String.py
@@ -0,0 +1,94 @@
+"""
+3. Longest Substring Without Repeating Characters   
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+"""
+
+class Solution:
+    # TAKES TOO LONG
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     max_str = 0
+    #     len_str = len(s)
+    #     print(len_str)
+    #     for n in reversed(range(len_str)):
+    #         # No need to check for one character length
+    #         if n == 0:
+    #             max_str = 1
+    #             return max_str
+            
+    #         for i in range(len_str - n):
+    #             sub_str = s[i:i + n + 1]
+    #             if len(list(sub_str)) == len(set(sub_str)):
+    #                 max_str = n + 1
+    #                 return max_str
+
+    #     return max_str
+        
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        len_str = len(s)
+        print(len_str)
+        char_dict = {}
+        max_str = 0
+        last_match = -1
+        for idx, char in enumerate(s):
+            # Check dictionary for this char
+            if char in char_dict and last_match <= char_dict[char]:
+                last_match = char_dict[char] # Update last matched location
+            max_str = max(max_str, idx - last_match)
+            char_dict[char] = idx
+
+        return max_str
+
+def test_case(s: str):
+    ans = Solution().lengthOfLongestSubstring(s)
+    print(ans)
+
+if __name__ == "__main__":
+    test_case("tetabct")
+    a = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]
+        ^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>
+        ?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./
+        :;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345 6789!\"#$%&'()
+        *+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$
+        %&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+        !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234
+        56789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU
+        VWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP
+        QRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJK
+        LMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEF
+        GHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzA
+        BCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuv
+        wxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopq
+        rstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijkl
+        mnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefg
+        hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ab
+        cdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|
+        }~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]
+        ^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?
+        """ * 20
+    test_case(a)
+
+"""
+Runtime:      76 ms, faster than 78.96% of Python3 online submissions for 
+              Longest Substring Without Repeating Characters.
+Memory Usage: 13.6 MB, less than 5.05% of Python3 online submissions for 
+              Longest Substring Without Repeating Characters.
+"""
